ea53af97ed2b399720e4d5bbc6ca28dd1a0f3bd9
Alec Dewulf <alecdewulf@Alecs-MacBook-Air.local>
2021-11-01 16:37:07 -0400

    implemented controller

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+motor l1 = motor(PORT20);
+motor l2 = motor(PORT19);
+motor r1 = motor(PORT10);
+motor r2 = motor(PORT9);
+
+
+// A global instance of competition
+competition Competition;
+
+void pre_auton(void) {
+  vexcodeInit();
+}
+
+void autonomous(void) {
+  double speed = 70;
+  l1.spin(directionType::fwd, speed, percentUnits::pct);
+  
+}
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (true) {
+
+    l1.spin(directionType::fwd, Controller1.Axis2.value(), percentUnits::pct);
+    l2.spin(directionType::rev, Controller1.Axis2.value(), percentUnits::pct);
+    
+    r1.spin(directionType::rev, Controller1.Axis2.value(), percentUnits::pct);
+    r2.spin(directionType::fwd, Controller1.Axis2.value(), percentUnits::pct);
+
+
+    wait(20, msec); 
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+  usercontrol();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
